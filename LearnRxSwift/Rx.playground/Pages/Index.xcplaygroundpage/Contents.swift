6d806a10826fed0de9edd1ca2ad976e8fd7f4b2c
DianQK <xiaoqing@dianqk.org>
2016-05-13 11:13:26 +0800

    Remove unused MVVM

--- LearnRxSwift/Rx.playground/Pages/Index.xcplaygroundpage/Contents.swift
+++ LearnRxSwift/Rx.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -23,3 +23,34 @@
 */
 
 //: [目录](Index) - [下一节 >>](@next)
+
+import RxSwift
+
+
+class Foo {
+    let bar: Int
+    
+    init(bar: Int) {
+        self.bar = bar
+    }
+    
+    deinit {
+        print("Deinit \(bar)")
+    }
+}
+
+
+
+
+enum ErrorSS: Int, ErrorType {
+    case Foo = 2
+    
+    var _code: Int {
+        return rawValue
+    }
+}
+
+
+print(ErrorSS.Foo._code)
+
+
